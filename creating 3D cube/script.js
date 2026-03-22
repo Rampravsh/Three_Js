265479914f5b451f93c577ea48a8bc48b1d295cc
Rampravesh <rampraveshkr4545@gmail.com>
2026-03-22 14:04:55 +0530

    creating 3D cube with three.js

--- creating 3D cube/script.js
+++ creating 3D cube/script.js
@@ -3,28 +3,27 @@ const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
-  1000,
+  100,
 );
 
 const canvas = document.getElementById("draw");
 
-const renderer = new THREE.WebGLRenderer({canvas});
+const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshBasicMaterial({ color: "rgba(255,0,0,0.1)" });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+cube.scale.x = 2;
 
 camera.position.z = 5;
 
 function animate(time) {
-  cube.rotation.x = time / 2000;
+  //   cube.rotation.z = time / 200;
   cube.rotation.y = time / 1000;
 
   renderer.render(scene, camera);
 }
-
-animate()
